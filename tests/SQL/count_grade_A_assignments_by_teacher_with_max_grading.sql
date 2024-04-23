ab65bb0489c254259e7fd7c46c71b877597d7170
Karthikeya <yelisettikarthik0@gmail.com>
2024-04-23 05:45:29 +0530

    setup sql tests
    the files modified have an sql command to evaluate the respective tests

--- tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
+++ tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
@@ -1,1 +1,11 @@
 -- Write query to find the number of grade A's given by the teacher who has graded the most assignments
+SELECT COUNT(grade) AS grade_A_count
+FROM assignments JOIN (
+    SELECT teacher_id
+    FROM assignments
+    WHERE state = 'GRADED'
+    GROUP BY teacher_id
+    ORDER BY COUNT(grade) DESC
+    LIMIT 1
+) AS max_teach ON assignments.teacher_id = max_teach.teacher_id
+WHERE grade = 'A';
